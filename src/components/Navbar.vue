1c8dad82279a0a90ee57549d3809a0406427eaff
Raíssa Dias <raissablw@gmail.com>
2024-05-22 20:43:21 -0300

    default files removed, route files added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="nav">
+        <RouterLink to="/">
+            <img src="" alt="Logo - Make Ur Burger" id="logo">
+        </RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/order">Orders</RouterLink>
+    </div>
+</template>
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/OrderView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="order">
+    <h1>Orders</h1>
+  </div>
+</template>
+
+<style>
+</style>
