ccbdbd3b4f59d31e650d7f8c412db73366112162
Raíssa Dias <raissablw@gmail.com>
2024-05-22 21:34:40 -0300

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,30 @@
 <template>
-    <p>Footer</p>
+    <footer id="footer">
+        <p>Make Ur Burger &copy; {{ year }}.</p>
+    </footer>
 </template>
 
 <script>
 export default {
-    name: "Footer"
+    name: "Footer",
+    data() {
+        return {
+            year: new Date().getFullYear()
+        }
+    }
 }
 </script>
 
 <style scoped>
+    #footer {
+        background: #222;
+        height: 100px;
+        color: #e3a00e;
+        font-weight: 500;
+        letter-spacing: 2px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        bottom: 0;
+    }
 </style>
